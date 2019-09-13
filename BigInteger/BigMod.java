4f00974ae5d20c317b1961d190b13243739036e3
Sakib Alamin <55255186+sakiib@users.noreply.github.com>
2019-09-14 01:55:46 +0600

    Create BigMod.java

---
+++ BigInteger/BigMod.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.math.BigInteger;
+
+public class BigMod {
+    public static void main( String args[] ) {
+        Scanner scan = new Scanner( System.in );
+        int t = scan.nextInt();
+        for( int tc = 1; tc <= t; tc++ ) {
+            BigInteger base = BigInteger.ZERO;
+            BigInteger power = BigInteger.ZERO;
+            BigInteger mod = BigInteger.ZERO;
+            base = scan.nextBigInteger();
+            power = scan.nextBigInteger();
+            mod = scan.nextBigInteger();
+            System.out.println( BigMod( base , power , mod ) );
+        }
+        System.exit( 0 );
+    }
+    static BigInteger BigMod( BigInteger b , BigInteger p , BigInteger m ) {
+        return b.modPow( p , m );
+    }
+}
